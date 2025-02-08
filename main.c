85306b747d9f1932c0b0f46b89d7677afb4dea10
Gael Herrera <hr.emii87@gmail.com>
2025-02-08 02:09:55 -0800

    Sample and main compiler flow draft in main c file

--- main.c
+++ main.c
@@ -1,7 +1,81 @@
 #include <stdio.h>
 
 int main(int argc, char** argv) {
-    printf("Compiling...\n");
+    if (argc == 1) {
+        printf("Printing help...\n");
+        return 0;
+    }
+
+    Cli cli = cli_init(&Commands, argc, argv);
+
+    FILE* source_file = fopen(cli_get(&cli, "filename"), "r");
+    Status status = {0};
+
+    Reader source_reader = reader_init(source_file);
+    Tokenizer tokenizer = tokenizer_init(&source_reader, &status);
+
+    Ast ast = ast_init();
+
+    Parser parser = parser_init(&ast);
+    Analyzer analyzer = analyzer_init(&ast);
+
+    foreach (Token, token, tokenizer) {
+        /**
+         * Mutates the abstract syntax tree (AST) or returns an error if there
+         * was one.
+         */
+        status = parser_handle(&parser, token);
+
+        if (status.code != 0) {
+            return status.code;
+        }
+
+        status = analyzer_handle(&analyzer, token);
+
+        if (status.code != 0) {
+            return status.code;
+        }
+    }
+
+    /**
+     * From this part on the compiler has validated that the abstract syntax tree
+     * is valid. We could apply optimizations to it and mutate it before
+     * synthesizing the three address code.
+     */
+    // ast_optimize(&ast);
+
+    Writter tac_writter = writter_init(fopen("tac_temp", "w+"));
+
+    /**
+     * Synthesizes by chunks the abstract syntax tree to write the three address
+     * code.
+     */
+    tac_from_ast(&tac_writter, &ast);
+    /** Maybe also apply optimizations to the tac file generated */
+    // tac_optimize(&tac_writter);
+    writter_close(&tac_writter);
+
+    /**
+     * Synthesizes by chunks the three address code to assembly.
+     */
+    Writter asm_writter = reader_init(fopen("asm_temp", "w+"));
+    Reader tac_reader = reader_init(fopen("tac_temp", "r"));
+
+    asm_from_tac(&asm_writter, &tac_reader);
+    /** Maybe also apply optimizations to the assembly file generated */
+    // asm_optimize(&asm_writter);
+    writter_close(&asm_writter);
+    reader_close(&tac_reader);
+
+    Writter bin_writter = reader_init(fopen("a.out", "w+"));
+    Reader asm_reader = reader_init(fopen("asm_temp", "r"));
+
+    bin_from_asm(&bin_writter, &asm_reader);
+    writter_close(&bin_writter);
+    reader_close(&asm_reader);
+
+    remove("tac_temp");
+    remove("asm_temp");
 
     return 0;
 }
